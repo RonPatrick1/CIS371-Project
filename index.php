cc1d6d9f15c0a4d24f839ad8d4b0c824eb0458e1
Ron Patrick <ronpatrick@silveronstudios.com>
2016-06-18 23:52:02 -0400

    initial

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta http-equiv="Content-Type" content="text/html" charset="utf-8"/>
+    <title>Babylon - Getting Started</title>
+    <link rel="stylesheet" href="start.css" type="text/css"/>
+    <script src="babylon.2.3.js"></script>
+    <script src="scene.js"></script>
+
+</head>
+<body>
+<canvas id="renderCanvas"></canvas>
+</body>
+</html>
+
